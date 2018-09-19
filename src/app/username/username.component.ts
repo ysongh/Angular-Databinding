f41d6d1b1fae1bda5478f3999d995d5232233651
Song <yousonghou@yahoo.com>
2018-09-19 22:31:08 +0000

    refactoring button logic

--- src/app/username/username.component.ts
+++ src/app/username/username.component.ts
@@ -8,18 +8,4 @@ import { Component } from '@angular/core';
 export class UsernameComponent{
     username = '';
     isEmpty = false;
-    
-    onUpdateUsername(event: Event){
-        if(this.username !== ''){
-            this.isEmpty = true;
-        }
-        else{
-            this.isEmpty = false;
-        }
-    }
-    
-    onReset(){
-        this.username = '';
-        this.isEmpty = false;
-    }
 }
